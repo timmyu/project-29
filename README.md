8ac4c5183f9b47c66a58d9d79252b90fc4eee6e8
timmyu <53704495+timmyu@users.noreply.github.com>
2021-10-18 18:41:38 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-29
+# zombie-crush-boilerplate
+boilerplate for crush the zombie game
---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var stones =[]
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+  ground = new Base(0, height - 10, width * 2, 20);
+  leftWall = new Base(100, height - 300, 200, height / 2 + 100);
+  rightWall = new Base(width - 100, height - 300, 200, height / 2 + 100);
+
+  bridge = new Bridge(30, { x: 50, y: height / 2 - 140 });
+  jointPoint = new Base(width - 250, height / 2 - 100, 40, 20);
+
+  Matter.Composite.add(bridge.body, jointPoint);
+  jointLink = new Link(bridge, jointPoint);
+
+  for (var i = 0; i <= 8; i++) {
+    var x = random(width / 2 - 200, width / 2 + 300);
+    var y = random(-100, 100);
+    var stone = new Stone(x, y, 80, 80);
+    stones.push(stone);
+}
+}
+function draw() {
+  background(51);
+  Engine.update(engine);
+  bridge.show();
+ground.show()
+leftWall.show()
+rightWall.show()
+  for (var stone of stones) {
+    stone.show();
+  }    
+}
